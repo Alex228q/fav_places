909c76bc621138cb0e9208bc39ccf4d8f9b609bf
loki <youloki@example.com>
2024-10-02 22:03:48 +0300

    update02_10_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:favorite_places/screens/places_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
